952030f3a89be9b6adb2e1a5465762a13664079b
zhoumo01 <zhouzhx@gmail.com>
2015-11-04 17:39:24 +0800

    ch11 ajax ok

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,11 +30,14 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart,notice: 'Line item was successfully created.' }
+        format.html { redirect_to(store_url)}
+        format.js
         format.json { render :show, status: :created, location: @line_item }
+        format.xml {render :xml => @line_item, :status => :created, :location => @line_item}
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.xml{render :xml => @line_item.errors, :status => :unprocessable_entity}
       end
     end
   end
@@ -62,6 +65,8 @@ class LineItemsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104050719) do
+ActiveRecord::Schema.define(version: 20151104052140) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
